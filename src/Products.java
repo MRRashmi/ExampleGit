08d3704934aed8bd03ca3c5b9ce1a1f3e90cc7b0
MRRashmi <rashmi896mr@gmail.com>
2023-05-31 01:20:35 +0530

    b2commit

--- src/Products.java
+++ src/Products.java
@@ -1,9 +1,18 @@
 
 public class Products {
-	int id;
-	String name;
-	public Products(int id, String name) {
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
 		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
 		this.name = name;
 	}
+
+	
 }
